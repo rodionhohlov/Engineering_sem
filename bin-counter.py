e6f51a6653d8cdc8825c2853e4db43c5487b4862
rodionhohlov <139437054+rodionhohlov@users.noreply.github.com>
2026-03-05 13:42:03 +0300

    Add files via upload
    bin counth changed

--- bin-counter.py
+++ bin-counter.py
@@ -1,33 +1,46 @@
 import RPi.GPIO as GPIO
-import time 
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+
+up_button = 9
+down_button = 10
+
+for led in leds:
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led, 0)
+
+GPIO.setup(up_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(down_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+num = 0
 
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
-def printleds(leds, bin_num):
-    for i in range (len(leds)):
-        GPIO.output(leds[i], bin_num[i])
-
-GPIO.setmode(GPIO.BCM)
-
-leds = [16, 12, 25, 17, 27, 23, 22, 24]
-
-GPIO.setup(9, GPIO.IN)
-GPIO.setup(10, GPIO.IN)
-
-
-for i in leds:
-    GPIO.setup(i, GPIO.OUT)
-
-num = 0
 sleep_time = 0.2
-while 1:
-    if GPIO.input(up):
-        num = num + 1
+
+try:
+    while True:
+        if GPIO.input(up_button):
+            num = num + 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
         
-    elif GPIO.input(up):
-        num = num - 1
-    
-    print(num, dec2bin(num % 256))
-    time.sleep(sleep_time)
-    printleds(leds, dec2bin(num % 256))
+        if GPIO.input(down_button):
+            num = num - 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        
+        if num > 255:
+            num = 0
+        elif num < 0:
+            num = 255
+        
+        GPIO.output(leds, dec2bin(num))
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
--- double-touch.py
+++ double-touch.py
@@ -2,41 +2,48 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
+
+up_button = 9
+down_button = 10
+
 for led in leds:
     GPIO.setup(led, GPIO.OUT)
     GPIO.output(led, 0)
 
-up = 9
-down = 10
-GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(up_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(down_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 num = 0
-sleep_time = 0.2
 
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
-while True:
-    if GPIO.input(up) and GPIO.input(down):
-        num = 255
-        GPIO.output(leds, dec2bin(num))
-        time.sleep(sleep_time)
-    
-    elif GPIO.input(up):
-        num = num + 1
-        if num > 255:
+sleep_time = 0.2
+
+try:
+    while True:
+        if GPIO.input(up_button) and GPIO.input(down_button):
             num = 255
-        print(num, dec2bin(num))
-        GPIO.output(leds, dec2bin(num))
-        time.sleep(sleep_time)
-    
-    elif GPIO.input(down):
-        num = num - 1
-        if num < 0:
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        elif GPIO.input(up_button):
+            num = num + 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        elif GPIO.input(down_button):
+            num = num - 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        
+        if num > 255:
             num = 0
-        print(num, dec2bin(num))
+        elif num < 0:
+            num = 255
+        
         GPIO.output(leds, dec2bin(num))
-        time.sleep(sleep_time)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
